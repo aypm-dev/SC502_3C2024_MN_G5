d0ce05ceb05354848dd23709474678530ba9055c
AngelCode-dev <paniaguamoraa@gmail.com>
2024-12-17 10:00:02 -0600

    configuracion base de datos

--- db/Database.php
+++ db/Database.php
@@ -1,12 +1,14 @@
 <?php
-class Database {
+class Database
+{
     private $host = 'localhost';
-    private $db_name = 'tu_base_de_datos';
-    private $username = 'tu_usuario';
-    private $password = 'tu_contraseña';
+    private $db_name = 'lesco_project';
+    private $username = 'root';
+    private $password = '';
     private $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         try {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -15,7 +17,8 @@ class Database {
         }
     }
 
-    public function prepare($query) {
+    public function prepare($query)
+    {
         return $this->conn->prepare($query);
     }
 }
