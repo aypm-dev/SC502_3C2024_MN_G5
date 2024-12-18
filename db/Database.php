17f692fdd298677d6fdb1dd33764c1cf6742b8e8
joseAS1201 <111660346+joseAS1201@users.noreply.github.com>
2024-12-17 20:47:32 -0600

    funcion especifico para probar la conexion
    funcion especifico para probar la conexion

--- db/Database.php
+++ db/Database.php
@@ -1,14 +1,12 @@
 <?php
-class Database
-{
+class Database {
     private $host = 'localhost';
     private $db_name = 'lesco_project';
     private $username = 'root';
     private $password = '';
-    public $conn;
+    private $conn;
 
-    public function __construct()
-    {
+    public function __construct() {
         try {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -17,19 +15,17 @@ class Database
         }
     }
 
-    public function prepare($query)
-    {
+    public function prepare($query) {
         return $this->conn->prepare($query);
     }
+
+    public function testConnection() {
+        try {
+            $this->conn->query('SELECT 1');
+            return ['status' => 'success', 'message' => 'Connection successful.'];
+        } catch (PDOException $e) {
+            return ['status' => 'error', 'message' => $e->getMessage()];
+        }
+    }
 }
-
-// LINEAS DE PRUEBA, DESCOMENTAR PARA PROBAR LA CONEXION CON LA BASE DE DATOS
-
-// $db = new Database();
-
-// echo var_dump($db->conn);
-// $resultado = $db->prepare("select * from videollamadas");
-// $resultado->execute();
-
-// echo var_dump($resultado);
-// echo var_dump($resultado->fetchAll());
+?>
