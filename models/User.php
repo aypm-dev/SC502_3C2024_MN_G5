1c43a348d539813349293727cb7a34420d0c7a9c
AYPM <85044270+aypm-dev@users.noreply.github.com>
2024-12-17 23:14:49 -0600

    agregando funciones eliminadas accidentalmente en otros commits

--- models/User.php
+++ models/User.php
@@ -2,21 +2,25 @@
 
 require_once '../db/Database.php';
 
-class User {
+class User
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function register($name, $email, $password, $role_id) {
+    public function register($name, $email, $password, $role_id)
+    {
         $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
         $query = "INSERT INTO users (name, email, password, role_id) VALUES (?, ?, ?, ?)";
         $stmt = $this->db->prepare($query);
         return $stmt->execute([$name, $email, $hashedPassword, $role_id]);
     }
 
-    public function login($email, $password) {
+    public function login($email, $password)
+    {
         $query = "SELECT * FROM users WHERE email = ?";
         $stmt = $this->db->prepare($query);
         $stmt->execute([$email]);
@@ -25,14 +29,37 @@ class User {
         if ($user && password_verify($password, $user['password'])) {
             return $user;
         }
-        return null; 
+        return null;
     }
 
-    public function getAllUsers() {
+    public function getAllUsers()
+    {
         $query = "SELECT * FROM users";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllOnlineTranslators()
+    {
+        $query = "  SELECT usuario.id_usuario, usuario.nombre, usuario.correo, traductor.especialidad, traductor.disponibilidad
+                    FROM usuarios AS usuario
+                    INNER JOIN traductores AS traductor 
+                        ON usuario.id_usuario = traductor.id_traductor
+                    WHERE usuario.tipo_usuario = 'traductor' 
+                    AND traductor.disponibilidad = 'disponible';";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function agregarminutosausuario($minutos, $id_usuario)
+    {
+        $query = "UPDATE usuarios
+SET minutos = minutos + ?  -- Sumar 30 minutos, cambia el valor según lo que necesites
+WHERE id_usuario = ? ;  -- Cambia el id_usuario por el que desees actualizar";
+        $stmt = $this->db->prepare($query);
+        return $stmt->execute([$minutos, $id_usuario]);
+
     }
 }
 ?>
